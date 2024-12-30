1fbfdca2c3625d4b87cb4960475edd8895b83be1
Erin Kerr <erin.kerr17@gmail.com>
2024-12-29 18:36:48 -0800

    add pages and layout for dashboard

---
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return <p>Dashboard Page</p>;
+}
+
+// This is how you can create different pages in Next.js:
+// create a new route segment using a folder, and add a page file inside it.
+
+// it also allows you to colocate UI components
+// ex: /ui and /lib are colocated inside of the /app folder along with the routes
